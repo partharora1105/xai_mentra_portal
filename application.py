9fd28a0ec752dbe9f6f8f26b8f14ff2f6af9e477
Parth Arora <partharora@lawn-128-61-29-149.lawn.gatech.edu>
2024-05-01 15:13:23 -0400

    Made tree expandable

--- application.py
+++ application.py
@@ -8,11 +8,12 @@ from flask import Flask, redirect, url_for, request, render_template, send_from_
 import sqlite3
 from flask import g
 import json
+import re
 
 
 
 app = Flask(__name__, static_folder="static")
-localDomain = "http://localhost:5000"
+localDomain = "http://127.0.0.1:5000/"
 publicDomain = "http://www.gtxr.club"
 DOMAIN = localDomain
 
@@ -21,25 +22,103 @@ publicPath = "/home/GTXR/mysite/"
 PATH = localPath
 
 extension = ""
+root_key = "My Profile"
+
+
+def is_positive_integer(value):
+    try:
+        int_value = int(value)
+        return int_value > 0
+    except (ValueError, TypeError):
+        return False
+
+app.jinja_env.filters['is_positive_integer'] = is_positive_integer
+
 
 @app.route("/")
 def openHome():
     json_file = PATH + f"static/data/nodes.json"
     with open(json_file, 'r') as file:
         data = json.load(file)
+    for k,v in data[root_key].items():
+        data[root_key][k] = len(v)
+    json_file = PATH + f"static/data/curr_tree.json"
+    with open(json_file, 'w') as file:
+        json.dump(data, file, indent=4)
+
+    return render_template("index.html", nodes = data, domain=DOMAIN)
+
+
+def get_leaves_and_parents(tree, leaves, parent_map, path=[]):
+    for key, value in tree.items():
+        if type(value) == int or not value:  # Check if the node is a leaf
+            leaves.append(key)
+            parent_map[key] = path[:]
+        else:
+            path.append(key)
+            get_leaves_and_parents(value, leaves, parent_map, path)
+            path.pop()
+
+@app.route("/expand/expand/<k>")
+def doubleExpandTree(k):
+    return redirect(url_for('expandTree', k=k))
+
+
+@app.route("/expand/<k>")
+def expandTree(k):
+    json_file = PATH + f"static/data/curr_tree.json"
+    with open(json_file, 'r') as file:
+        curr_tree = json.load(file)
+    leaves = []
+    parent_map = {}
+    get_leaves_and_parents(curr_tree, leaves, parent_map)
+    try:
+        path = parent_map[k]
+    except KeyError:
+        return redirect(url_for('openHome'))
+
+
+    json_file = PATH + f"static/data/nodes.json"
+    with open(json_file, 'r') as file:
+        nodes = json.load(file)
+
+    curr = nodes[root_key]
+    for key in path[1:]:
+        curr = curr[key]
+    curr = curr[k]
+    for key,val in curr.items():
+        curr[key] = len(val)
+    tree_to_expand = curr
+    
+    print(tree_to_expand)
+    current_node = curr_tree
+    for key in path:
+        current_node = current_node[key]
+    current_node[k] = tree_to_expand
+
+    json_file = PATH + f"static/data/curr_tree.json"
+    with open(json_file, 'w') as file:
+        json.dump(curr_tree, file, indent=4)
+    data = curr_tree
+
+    return render_template("index.html", nodes = data, domain=DOMAIN)
+
+
+
+    # Print the parent map
+    print("Parent map:")
+    for leaf, parents in parent_map.items():
+        print(leaf, ":", " -> ".join(parents))
+
+
+    return leaves
+    json_file = PATH + f"static/data/nodes.json"
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+    for k,v in data[root_key].items():
+        data[root_key][k] = len(v)
     data = dict(data)
     return render_template("index.html", text="Hello World", nodes = data, domain=DOMAIN)
-
-
-# @app.route(f"{extension}/admin/SH", methods=["POST", "GET"])
-# def admin():
-#     json_file = PATH + f"static/user_data/tracking.json"
-#     with open(json_file, 'r') as file:
-#         data = json.load(file)
-#     date = datetime.datetime.now().strftime('%Y-%m-%d')
-#     week_before = datetime.datetime.strptime(date, '%Y-%m-%d') - datetime.timedelta(days=7)
-#     return render_template('admin.html', data=data, current_date= date ,
-#                            week_before= week_before.strftime('%Y-%m-%d')  ,domain=DOMAIN)
 
 
 if __name__ == "__main__":
